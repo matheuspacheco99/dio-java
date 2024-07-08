b36746a01a61d42f203f89dfaeaefdbf0eb27425
matheuspacheco99 <matheussouzap99@gmail.com>
2024-07-08 19:18:03 -0300

    testando o terminal e abrindo arquivos com CMD

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse { 
+    public static void main(String[] args){
+        System.out.println("Oi, fui executado pelo terminal! De novo");
+        //no CMD use o comando cd para entrar na pasta dos arquivos
+        //use o comando: java [nome do arquivo] e ele executara
+   }
+}
